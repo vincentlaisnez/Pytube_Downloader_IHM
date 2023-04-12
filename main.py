21847d52b7b94336021527786e7e896de12967b9
thecrow <crow.manga@gmail.com>
2023-04-12 21:43:08 +0200

    - correction QProgressDialog - add check if only 1 url --> destroy ProgressDialog - add if download finish, clear ListWidget and Message Box - add delete selection.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PySide6 import QtCore
 from PySide6.QtCore import QObject, QThread
+from PySide6.QtGui import QKeySequence, QShortcut
 from PySide6.QtWidgets import QApplication, QWidget, QLabel, QListWidget, QLineEdit, QPushButton, QProgressBar, \
-    QVBoxLayout, QProgressDialog
+    QVBoxLayout, QProgressDialog, QMessageBox
 
 from sounds_download import directory_change, download
 
@@ -13,6 +14,7 @@ class Worker(QObject):
     def __init__(self, url_to_download):
         super().__init__()
         self.url_to_download = url_to_download
+        self.finish = False
 
     def url_download(self):
         for url in self.url_to_download:
@@ -20,6 +22,7 @@ class Worker(QObject):
             self.url_downloaded.emit(url, success)
 
         self.finished.emit()
+        self.finish = True
 
 
 class MainWindow(QWidget):
@@ -59,6 +62,7 @@ class MainWindow(QWidget):
         self.main_layout.addWidget(self.bn_dl)
 
     def setup_connections(self):
+        QShortcut(QKeySequence("Delete"), self.lw_urls, self.delete_selected_items)
         self.le_url.returnPressed.connect(self.add_url_to_list)
         self.bn_dl.clicked.connect(self.lunch_download)
 
@@ -74,7 +78,7 @@ class MainWindow(QWidget):
         self.worker.finished.connect(self.thread.quit)
         self.thread.start()
 
-        self.prg_dialog = QProgressDialog("téléchargement en cours... ", "Ok", 0, len(self.items))
+        self.prg_dialog = QProgressDialog("téléchargement en cours... ", "Annuler", 1, len(self.items))
         self.prg_dialog.canceled.connect(self.abort)
         self.prg_dialog.show()
 
@@ -87,7 +91,19 @@ class MainWindow(QWidget):
 
     def url_to_download(self):
         self.prg_dialog.setValue(self.prg_dialog.value() + 1)
-        print("ok")
+        if len(self.items) == 1:
+            self.prg_dialog.destroy()
+
+        if self.worker.finish:
+            self.lw_urls.clear()
+            msg_box = QMessageBox()
+            msg_box.setText("Musique(s) téléchargée(s)")
+            msg_box.exec()
+
+    def delete_selected_items(self):
+        for lw_item in self.lw_urls.selectedItems():
+            row = self.lw_urls.row(lw_item)
+            self.lw_urls.takeItem(row)
 
 
 if __name__ == '__main__':
